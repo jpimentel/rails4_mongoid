db3071e4717a2cd476d0f1e000801a37faa6c020
Josue Pimentel <josue812@gmail.com>
2015-09-28 19:24:22 -0400

    adding comments to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,18 @@ class User
   index({ username: 1 } , { unique: true })
 
   def comments
-    []
+
+    user_id = _id.to_s
+    user_comments = []
+
+    Post.where('comments.user_id' => user_id).each do |post|
+      post.comments.each do |comment|
+        if comment.user_id == user_id
+          user_comments.append(comment)
+        end
+      end
+    end
+
+    user_comments
   end
 end
